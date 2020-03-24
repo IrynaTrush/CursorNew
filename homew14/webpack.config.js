ab5ff8bf8180dbccbda3ff08196beb611892c63c
Iryna <irynka1071994@gmail.com>
2020-03-24 19:20:01 +0200

    homew14

--- homew14/webpack.config.js
+++ homew14/webpack.config.js
@@ -8,9 +8,7 @@ module.exports = {
             exclude: /(node_modules|bower_components)/,
             use: {
               loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
+              
             }
           },
           {
